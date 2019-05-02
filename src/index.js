ec68e6b6a64a2b918312bf142e41b171443a23dd
akmaldevtron <m.akmal.khan@devtrongenesis.com>
2019-05-02 17:13:38 +0500

    Basic Code Of React Is Removed

--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import JSON from './db.json';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Components
-import Header from './components/header'
-import NewsList from './components/new_list'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class App extends Component {
-    state = {
-        news: JSON,
-        filtered: []
-    }
-    getKeyword = (event) => {
-        // console.log(event.target.value)
-        let keyword = event.target.value;
-        let filtered = this.state.news.filter((item) => {
-            return item.title.indexOf(keyword) > -1
-        });
-        this.setState({
-            filtered
-        })
-    }
-    render() {
-        let newsFiltered = this.state.filtered;
-        let newsWhole = this.state.news
-        return (
-            <div>
-                <Header keywords={this.getKeyword} />
-                <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}>
-                    <h3>The news are:</h3>
-                </NewsList>
-            </div>
-        )
-    }
-}
-
-
-ReactDOM.render(<App/>, document.querySelector('#root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
